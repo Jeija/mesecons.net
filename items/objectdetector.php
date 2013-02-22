2ab9b28dba1f9abeb1a9a9c42d9336ed0b85a5dd
Jeija <norrepli@gmail.com>
2013-02-22 20:01:30 +0100

    Update information and craft recipe of vertical wires and object detector

--- items/objectdetector.php
+++ items/objectdetector.php
@@ -6,7 +6,9 @@
 	<br/><br/>
 	<div class = "paragraph-heading">Description</div>
 	<div class = "text">
-		The object detector is a receptor. It changes its state when an object approaches, it detects players as well as items. Also powers the two blocks below it. If a sign is above the detector, it will only turn on if the player's name is the text on the sign.
+		The object detector is a receptor. It changes its state when a player approaches.<br/>
+		Right-click it to set a name to scan for.<br/>
+		It can also receive digiline signals which are the name to scan for on the specified channel in the right-click menu.
 	</div>
 </div>
 <?php include "../footer.html";?>
--- items/vertical.php
+++ items/vertical.php
@@ -2,8 +2,7 @@
 <div id = "article">
 	<div class = "article-heading">Vertical Mesecon Wire</div><br/>
 	<div class = "paragraph-heading">Craft recipe</div>
-	<div class = "text">There is no recipe yet :(<br/>
-	You can send a pull request and add one :D</div>
+	<img src="/crafts/vertical.png">
 	<br/>
 	<div class = "paragraph-heading">Description</div>
 	<div class = "text">
