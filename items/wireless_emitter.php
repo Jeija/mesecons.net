f22b1ccd6bfda9e99ae679d419ce591aea346e2e
Novatux <nathanael.courant@laposte.net>
2013-06-19 19:01:30 +0200

    Add documentation about wireless emitters/receivers.

---
+++ items/wireless_emitter.php
@@ -0,0 +1,12 @@
+<?php include "../header.html";?>
+<div id = "article">
+  <div class = "article-heading">Wireless emitter</div><br/>
+  <div class = "paragraph-heading">Craft recipe</div>
+  <img src = "../crafts/wireless_emitter.png">
+  <br/><br/>
+  <div class = "paragraph-heading">Description</div>
+  <div class = "text">
+    Wireless emitter must be used with a wireless receiver to work. You can configure a channel in its GUI, and wireless receivers with the same channel will be powered and unpowered when the emitter will be powered and unpowered.
+  </div>
+</div>
+<?php include "../footer.html";?> 
