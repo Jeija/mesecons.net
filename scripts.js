a036275610e7c0f9ff7b1dda23cdec97ed02a5e7
Jeija <jeija@mesecons.net>
2014-01-04 16:29:05 +0100

    Update footer and remove Google+ +1 button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,1 @@
-//+1 Button
-(function() {
-	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	po.src = 'https://apis.google.com/js/plusone.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
+// empty, previously Google +1 button was here
